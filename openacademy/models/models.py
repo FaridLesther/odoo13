88648be3495ee6e0a4df37281eb3edbf9f345ff7
FaridLesther <61443299+FaridLesther@users.noreply.github.com>
2021-02-09 22:16:56 -0600

    [Nuevo]Modelo curso

---
+++ openacademy/models/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# modelos para tablas de la base de datos
+from odoo import models, fields, api
+
+
+class Course(models.Model):
+    #
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
