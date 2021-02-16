61c8bb9ba4de65797c32821b02b68418bafe334a
FaridLesther <faridlesther@yahoo.es>
2021-02-16 00:41:09 +0000

    [Modificado]reports,.xml course.py, session.py, etiquetas

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -8,24 +8,24 @@ class Course(models.Model):
     _name = 'openacademy.course'
     _description = "OpenAcademy Courses"
 
-    name = fields.Char(string="Title", required=True)
+    name = fields.Char(string="Titulo", required=True)
     description = fields.Text()
 
-    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
-    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsable", index=True)
+    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sesiones")
 
     def copy(self, default=None):
-            default = dict(default or {})
+        default = dict(default or {})
 
-            copied_count = self.search_count(
-                [('name', '=like', _(u"Copy of {}%").format(self.name))])
-            if not copied_count:
-                new_name = _(u"Copy of {}").format(self.name)
-            else:
-                new_name = _(u"Copy of {} ({})").format(self.name, copied_count)
+        copied_count = self.search_count(
+            [('name', '=like', _(u"Copy of {}%").format(self.name))])
+        if not copied_count:
+            new_name = _(u"Copy of {}").format(self.name)
+        else:
+            new_name = _(u"Copy of {} ({})").format(self.name, copied_count)
 
-            default['name'] = new_name
-            return super(Course, self).copy(default)
+        default['name'] = new_name
+        return super(Course, self).copy(default)
 
     _sql_constraints = [
         ('name_description_check',
--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -1,30 +1,31 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from datetime import timedelta
+from odoo import models, fields, api, exceptions, _
 
 class Session(models.Model):
     _name = 'openacademy.session'
     _description = "OpenAcademy Sessions"
 
-    name = fields.Char(required=True)
-    start_date = fields.Date(default=fields.Date.today)
-    duration = fields.Float(digits=(6, 2), help="Duration in days")
-    seats = fields.Integer(string="Number of seats")
-    active = fields.Boolean(default=True)
+    name = fields.Char(string="Nombre", required=True)
+    start_date = fields.Date(string="Fecha Inicio", default=fields.Date.today)
+    duration = fields.Float(string="Duración", digits=(6, 2), help="Duración en días")
+    seats = fields.Integer(string="Número de plazas")
+    active = fields.Boolean(string="Activo", default=True)
     color = fields.Integer()
 
     instructor_id = fields.Many2one('res.partner', string="Instructor",
         domain=['|', ('instructor', '=', True), ('category_id.name', 'ilike', "Teacher")])
 
     course_id = fields.Many2one('openacademy.course',
-        ondelete='cascade', string="Course", required=True)
+        ondelete='cascade', string="Curso", required=True)
 
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    attendee_ids = fields.Many2many('res.partner', string="Participantes")
 
     #Campos calculados
-    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
-    end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+    taken_seats = fields.Float(string="Plazas ocupadas", compute='_taken_seats')
+    end_date = fields.Date(string="Fecha fin", store=True, compute='_get_end_date', inverse='_set_end_date')
 
-    attendees_count = fields.Integer(string="Attendees count", compute='_get_attendees_count', store=True)
+    attendees_count = fields.Integer(string="Número de Participantes", compute='_get_attendees_count', store=True)
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -39,15 +40,15 @@ class Session(models.Model):
         if self.seats < 0:
             return {
                 'warning': {
-                    'title': _("Incorrect 'seats' value"),
-                    'message': _("The number of available seats may not be negative"),
+                    'title': _("Valor de plazas incorrecto"),
+                    'message': _("El número disponible de plazas no puede ser negativo"),
                 },
             }
         if self.seats < len(self.attendee_ids):
             return {
                 'warning': {
-                    'title': _("Too many attendees"),
-                    'message': _("Increase seats or remove excess attendees"),
+                    'title': _("Hay más Participantes que plazas disponibles"),
+                    'message': _("Incremente las plazas o borre el exceso de Participantes"),
                 },
             }
 
@@ -81,4 +82,4 @@ class Session(models.Model):
     def _check_instructor_not_in_attendees(self):
         for r in self:
             if r.instructor_id and r.instructor_id in r.attendee_ids:
-                raise exceptions.ValidationError(_("A session's instructor can't be an attendee"))
+                raise exceptions.ValidationError(_("Un instructor de sesión no puede ser un participante"))
